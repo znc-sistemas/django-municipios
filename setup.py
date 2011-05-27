dc77d020909d4d73cfd2819761724e89ca9b4cbb
cadu <cadu@cadu-desk-linx.(none)>
2011-05-26 23:19:18 -0300

    encode!

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
  
 setup(
-    name='Django-Municipios',
-    version=VERSION,
+    name='django-municipios',
+    version='0.5',
     description='Aplicação plugável Django com modelos e widgets para os Municípios Brasileiros',
     long_description=open('README.rst').read(),
     author='ZNC Sistemas',
@@ -11,20 +12,17 @@ setup(
     maintainer_email='contato@znc.com.br',
     url='https://github.com/znc-sistemas/django-municipios',
     packages=find_packages(),
-    package_data=package_data,
     zip_safe=False,
     classifiers=[
         'Framework :: Django',
-        'Development Status :: 3 - Production/Stable',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        "Intended Audience :: System Administrators",
-        "Topic :: Software Development"
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.4',
         'Programming Language :: JavaScript',
+        'Topic :: Utilities',
+        'Natural Language :: Portuguese (Brazilian)'
     ],
 )
