e88efa06a7651adccba3422fae8ee15fc5460b23
ZNC Sistemas <git@znc.com.br>
2011-07-15 19:48:43 -0300

    Adicionando management comand para importação de dados de shapefile do IBGE

---
+++ municipios/management/commands/convertibgeshape.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand, CommandError
+
+from municipios.utils.ibge import convert_shapefile
+
+class Command(BaseCommand):
+    args = '<IBGE shapefile>'
+    help = u'Importa shapefile do IBGE para os modelos UF e Municipio'
+    
+    def handle(self, *args, **options):
+        if not args:
+            raise CommandError('Arquivo Shapefile do IBGE não especificado!')
+        print "Convertendo '%s':" % args[0]
+        convert_shapefile(args[0])
