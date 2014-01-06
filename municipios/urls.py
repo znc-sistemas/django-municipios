32e68a87ff2a37149254e449e080ad11ca7ac69a
Marcelo Tambalo <marcelo.zokis@gmail.com>
2014-01-06 17:07:38 -0200

    pep8

--- municipios/urls.py
+++ municipios/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('municipios.views',
-	url(r'^$', 'base_url_js', name='municipios-base-url'),
-	url(r'^base_url.js$', 'base_url_js', name='municipios-base-url-js'),
-    url(r'^ajax/municipios/(?P<uf>\w\w)/$', 'municipios_ajax', name='municipios-ajax'),   
+urlpatterns = patterns(
+    'municipios.views',
+    url(r'^$', 'base_url_js', name='municipios-base-url'),
+    url(r'^base_url.js$', 'base_url_js', name='municipios-base-url-js'),
+    url(r'^ajax/municipios/(?P<uf>\w\w)/$', 'municipios_ajax', name='municipios-ajax'),
     url(r'^teste/', 'teste', name='municipios-teste'),
 )
