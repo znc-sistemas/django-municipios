67b5f809fb74260f7a9ac7348d6690fed629d188
Marcelo Tambalo <marcelo@znc.com.br>
2016-03-17 14:59:26 -0300

    adiciona app-label e object-model ao widget

--- municipios/widgets.py
+++ municipios/widgets.py
@@ -6,13 +6,13 @@ from django.template.loader import get_template
 
 from django.utils.safestring import mark_safe
 
+from django.apps import apps
+
 try:
     from django.core.urlresolvers import reverse_lazy
     HAS_REVERSE_LAZY = True
 except ImportError:
     HAS_REVERSE_LAZY = False
-
-from municipios.models import Municipio, UF
 
 
 class SelectMunicipioWidget(Widget):
@@ -21,6 +21,12 @@ class SelectMunicipioWidget(Widget):
     and a <select> with the actual Municipio depending on the state
     selected
     """
+    def __init__(self, *args, **kwargs):
+        self.app_label = kwargs.pop('app_label', 'municipios')
+        self.object_name = kwargs.pop('object_name', 'Municipio')
+        self.mun_cls = apps.get_model(self.app_label, self.object_name)
+        super(SelectMunicipioWidget, self).__init__(*args, **kwargs)
+
     def render(self, name, value, attrs=None):
         if value is None:
             value = ''
@@ -30,25 +36,32 @@ class SelectMunicipioWidget(Widget):
 
         output = []
         uf_val = ''
-        uf_choices = [('', '--')]+[(uf.pk, uf.uf) for uf in UF.objects.order_by('uf')]
+        uf_choices = [('', '--')]+list(self.mun_cls.objects.values_list('uf__id_ibge', 'uf_sigla').distinct().order_by('uf'))
         uf_select = Select(choices=uf_choices)
         municipio_select = Select(choices=(('', '--'),))
 
         if value:
             try:
-                municipio = Municipio.objects.get(pk=value)
+                municipio = self.mun_cls.objects.get(pk=value)
                 uf_val = municipio.uf.pk
-                mun_choices = [(m.pk, m.nome) for m in Municipio.objects.filter(uf=uf_val).order_by('nome')]
+                mun_choices = [(m.pk, m.nome) for m in self.mun_cls.objects.filter(uf=uf_val).order_by('nome')]
                 municipio_select = Select(choices=[('', '- Selecione -')]+mun_choices)
-            except Municipio.DoesNotExist:
+            except self.mun_cls.DoesNotExist:
                 pass
         uf_attrs = self.attrs.copy()
-        uf_attrs.update({"id": '%s_uf' % name, "onchange": "changeUF(this);"})
+        uf_attrs.update(
+            {
+                "id": '%s_uf' % name,
+                "onchange": "changeUF(this);",
+                "data-app_label": self.app_label,
+                "data-object_name": self.object_name,
+            }
+        )
         select_html = uf_select.render('%s_uf' % name, uf_val, attrs=uf_attrs)
         required = False
         if 'class' in self.attrs:
             required = self.attrs['class']
-        #output.append(u'<div class="field"><label%s>UF</label><br />%s</div>' % (required and ' class="required"' or '', select_html))
+        # output.append(u'<div class="field"><label%s>UF</label><br />%s</div>' % (required and ' class="required"' or '', select_html))
         template_uf = get_template('municipios/uf_field.html')
         context_uf = Context({'label_class': required, 'wselect': select_html})
         output.append(template_uf.render(context_uf))
@@ -56,7 +69,7 @@ class SelectMunicipioWidget(Widget):
         munic_attrs = self.attrs.copy()
         munic_attrs['style'] = "width:250px;"
         select_html = municipio_select.render(name, value, munic_attrs)
-        #output.append(u'<div class="field"><label%s>Município</label><br />%s</div>' % (required and ' class="required"' or '', select_html))
+        # output.append(u'<div class="field"><label%s>Município</label><br />%s</div>' % (required and ' class="required"' or '', select_html))
         template_mun = get_template('municipios/municipio_field.html')
         context_mun = Context({'label_class': required, 'wselect': select_html})
         output.append(template_mun.render(context_mun))
